b7ae7dc6d7e82c0da98d235363730051e26c6fb9
ahmed312005 <113601141+ahmed312005@users.noreply.github.com>
2022-12-22 13:53:00 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace project_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y;
+            Console.WriteLine("enter the first number");
+            x = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter the next number");
+            y = int.Parse(Console.ReadLine());
+
+            for (int i = x; i <= y; i++)
+            {
+                int z = 0;
+                for (int j = 1; j < i; j++)
+                {
+                    if (i % j == 0)
+                        z += j;
+                }
+                if (z == i)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
